a0670a2252e0e32c76ed5d2f4a565e204bf73f0b
Suhas Nadarge <nadargesuhas@gmail.com>
2022-08-17 20:07:36 +0100

    Stack exceed error fix

--- MyBooking/karma.conf.js
+++ MyBooking/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-bookings'),
+      dir: require('path').join(__dirname, './coverage/my-booking'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- MyBooking/src/app/app.component.spec.ts
+++ MyBooking/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MyBookings'`, () => {
+  it(`should have as title 'MyBooking'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyBookings');
+    expect(app.title).toEqual('MyBooking');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MyBookings app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('MyBooking app is running!');
   });
 });
